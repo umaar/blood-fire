4a588a3c1f07ad133a19b02b472db8bd04418aff
Umar Hansa <umar.hansa@gmail.com>
2017-08-20 21:37:53 +0100

    Development / prod urls

--- modules/fetch-all-data.js
+++ modules/fetch-all-data.js
@@ -3,11 +3,16 @@ import fs from 'fs';
 import zlib from 'zlib';
 import csv from 'fast-csv';
 
-import transformData from './/blood-line';
+import transformData from './blood-line';
 
-export default function fetchAllData() {
-	// const url = 'https://www.dropbox.com/sh/pprrtnz0v6zt978/AABtu7bXJ4Vk5pxhGnQtIjMXa/GlicemiaMisurazioni.csv.gz?dl=1';
-	const url = 'http://127.0.0.1:8080/GlicemiaMisurazioni.csv.gz';
+const productionURL = 'https://www.dropbox.com/sh/pprrtnz0v6zt978/AABtu7bXJ4Vk5pxhGnQtIjMXa/GlicemiaMisurazioni.csv.gz?dl=1';
+
+const developmentURL = 'http://127.0.0.1:8080/GlicemiaMisurazioni.csv.gz'
+
+export default function fetchAllData({isDevelopment}) {
+	const url = isDevelopment ? developmentURL : productionURL;
+
+	console.log(`Fetching new data (${url})`);
 
 	const storage = [];
 	const gunzip = zlib.createGunzip();
--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,44 @@ import Head from 'next/head'
 import fetchAllData from '../modules/fetch-all-data';
 import LandingSection from '../components/landing-section';
 
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+console.log(`[App Running] (${isDevelopment ? 'development' : 'production'})`);
+
 let cachedData = [];
+let lastRetrievedTime;
+
+function dataIsStale() {
+	const maxAgeForData = 1000 * 60 * 10; // Ten Minutes
+	const timeSinceUpdate = +(new Date()) - lastRetrievedTime;
+	const isStale = timeSinceUpdate > maxAgeForData;
+	console.log(`Data is ${isStale ? 'stale ☠' : 'fresh 🍎'} (${timeSinceUpdate}ms)`);
+	return isStale;
+}
 
 async function getAllReadings() {
-	if (cachedData.length) {
+	if (cachedData.length && !dataIsStale()) {
 		console.log('Returning cached data. Example: ', cachedData[0]);
 		return cachedData;
 	} else {
-		console.log('Fetching new data');
 		try {
-			cachedData = await fetchAllData();
+			cachedData = await fetchAllData({
+				isDevelopment
+			});
+
+			lastRetrievedTime = +(new Date());
+
 			return cachedData;
 		} catch (err) {
 			throw new Error(err);
 		}
 	}
 }
+
+// Populate with data on app start
+getAllReadings().catch(err => {
+	console.log('Failed to populate data on app start. Error: ', err);
+});
 
 export default class Index extends React.Component {
 	static async getInitialProps () {
